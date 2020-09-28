5e8d8011bf0fdf3a942da112d649d35a77f20254
reimon <reimonsk8@gmail.com>
2020-09-27 23:23:23 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="wrap">
+        <div class="inner">
+          <span>JOSE RIOS XYZ</span>
+        </div>
+      </div>
+
+    </div>
+    
+  );
+}
+
+export default App;
