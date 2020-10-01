c5c52b67236caf45fb8a7e8f1cbf55852f2d2513
Jose Ramón gomez armenta <j.armenta@samsung.com>
2020-09-30 22:58:55 -0700

    first day work

--- client/src/components/Page.js
+++ client/src/components/Page.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import './Page.css'
+import Photo1 from '../photos/HOME.jpg'
+import Photo2 from '../photos/FOTO 002.jpg'
+import Photo3 from '../photos/IMG_1788.jpg'
+
 
 class Page extends React.Component{
   state = {
@@ -14,50 +18,79 @@ class Page extends React.Component{
   render(){
     const content = this.state.showContent;
     let showComponent;//this is the conditional render
-    if(content == "Home") {
+    if(content === "Home") {
       showComponent = 
-      <div className="Page"onClick={ () => this.nextPage("Menu")}>  
-      {showComponent}
-       <div className="Wrap">
-           <div className="Inner">
-             <span><p>JOSE RIOS XYZ</p></span>
-           </div>
-         </div>
-         
-         <div className="Footer">
-           <p>
-             JOSE RIOS 2020. ALL RIGHTS RESERVED
-           </p>
-         </div>
+      <div className="Page">
+        <div className="Wrap">
+          <img alt="home" src={Photo1} />
+          <div className="Inner">
+            <span onClick={ () => this.nextPage("Menu")}>
+              <p>JOSÉ RÍOS XYZ</p>
+            </span>
+            <div className="Footer">
+              <p>©JOSE RIOS 2020. ALL RIGHTS RESERVED</p>
+            </div>
+        </div>
+        </div>
+        <div className="midline"></div>
      </div>
 
-    } else if(content == "Menu") {
-        showComponent = 
-          <div className="Menu">
-            <div className="Wrap">
-              <div className="Inner">
-                
+    }else if(content === "Menu") {
+      showComponent = 
+        <div className= "Menu">
+          <div className="topline"></div>
+          <div className="Wrap">
+          <img alt="menu" src={Photo2} />
+            <div className="Inner">
+              <span onClick={ () => this.nextPage("Home")}>
+                  <p>INICIO</p>
+                </span>
+              <span className="Right" onClick={ () => this.nextPage("Contact")} >
+                <p>FOTOGRAFÍA </p>
+              </span>
+              <span onClick={ () => this.nextPage("Contact")} >
+                <p>DISEÑO GRÁFICO </p>
+              </span>
+
+              <span className="Right" onClick={ () => this.nextPage("Contact")} >
+                <p>CONTACTO</p>
+              </span>
+
+            </div>
+          </div>
+          <div className="botline"></div>
+        </div>
+    }else if (content === "Contact"){
+      showComponent = 
+      <div className="Page">
+        <div className="topline"></div>
+        <div className="Wrap">
+        <img alt="contact" src={Photo3} />    
+            <div className="Inner">
+              <div className="Contact">
                 <span onClick={ () => this.nextPage("Home")}>
-                  <p>HOME</p>
+                  <p>INICIO</p>
                 </span>
-
-                <span className="Right">
-                  <p>PORTAFOLIO</p>
-                </span>
-
-                <span>
-                  <p>ABOUT</p>
-                </span>
-
-                <span className="Right">
-                  <p>CONTACT</p>
-                </span>
-
+                <div>
+                  Diseñador gráfico - Fotógrafo independiente
+                </div>
+                <div>
+                  centrado en la comunicación y la identidad visual.
+                </div>
+                <div className="Footer">
+                  <p>©JOSE RIOS 2020. ALL RIGHTS RESERVED</p>
+                </div>
+                <div>
+                  <a href="https://www.instagram.com/usernamepepe/">Joserios.xyz@gmail.com</a> 
+                </div>
+                <div>
+                  < a href="https://www.instagram.com/usernamepepe/">Instagram - Usernamepepe</a>
+                </div>
               </div>
             </div>
-
-
         </div>
+        <div className="botline"></div>
+      </div>
     }
 
     return(
